09ba7d7e4b44b270485db8c00a5d56f927225492
N Balgopal Patro <46865367+balgopal123@users.noreply.github.com>
2019-07-27 23:22:12 +0530

    This Is a simple php form

---
+++ PHP forms/welcome.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head></head>
+    <body>
+        <h1>Whuu...</h1>
+        Welcome <?php echo $_POST["name"]; ?> <br>
+        Your email is    : <?php echo $_POST["email"]; ?><br>
+        Your DOB is      : <?php echo $_POST["dob"] ?>
+    </body>
+</html>
